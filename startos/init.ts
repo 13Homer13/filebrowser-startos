76643bcddee7d1a97217a0dfc0308252f7379408
Matt Hill <mattnine@protonmail.com>
2025-03-27 14:33:14 -0600

    create db on install

--- startos/init.ts
+++ startos/init.ts
@@ -5,12 +5,23 @@ import { setInterfaces } from './interfaces'
 import { versions } from './versions'
 import { actions } from './actions'
 import { jsonFile } from './file-models/filebrowser.json'
-import { configDefaults } from './utils'
+import { configDefaults, mnt } from './utils'
 import { resetAdminUser } from './actions/resetAdminUser'
 
 // **** Install ****
 const install = sdk.setupInstall(async ({ effects }) => {
   await jsonFile.write(effects, configDefaults)
+
+  await sdk.runCommand(
+    effects,
+    { imageId: 'filebrowser' },
+    ['/filebrowser', 'config', 'init', '-c', `${mnt}/filebrowser.json`],
+    {
+      mounts: sdk.Mounts.of().addVolume('main', null, '/root', false).build(),
+    },
+    'setadmin',
+  )
+
   await sdk.store.setOwn(effects, sdk.StorePath.adminUserCreated, false)
   await sdk.action.requestOwn(effects, resetAdminUser, 'critical', {
     reason: 'Create your admin user password',
